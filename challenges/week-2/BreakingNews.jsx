c24b3b676c1ccdba9a71bcc4d9e2ac565bed966c
AniaPiWo <snowflake@wp.pl>
2023-05-15 17:55:12 +0200

    tets

--- challenges/week-2/BreakingNews.jsx
+++ challenges/week-2/BreakingNews.jsx
@@ -9,7 +9,7 @@ export const BreakingNews = () => {
       <div className={css.newsBox}>
         <Marquee autoFill={true}>
         <div className={css.newsText}>
-        Compared to the hearing of humans, the hearing ability of cows is better in the deep and high frequency ranges. Compared to the hearing of humans, the hearing ability of cows is better in the deep and high frequency ranges.
+            Compared to the hearing of humans, the hearing ability of cows is better in the deep and high frequency ranges. Compared to the hearing of humans, the hearing ability of cows is better in the deep and high frequency ranges.
         </div>
         </Marquee>
       </div>
