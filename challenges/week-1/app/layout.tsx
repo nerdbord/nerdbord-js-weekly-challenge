fe8c67811a241940a394abcc8d46760414064b0e
Misiorny <krzystan@wp.pl>
2023-05-18 10:18:06 +0200

    feat: next.js added and initial commit

--- challenges/week-1/app/layout.tsx
+++ challenges/week-1/app/layout.tsx
@@ -1,13 +1,17 @@
 import { ReactNode } from 'react'
 
-export default function RootLayout({
+export default function RootLayout ({
   children,
 }: {
   children: ReactNode;
 }) {
   return (
     <html lang="en">
+      <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+        <title>Blinking Stars</title>
+      </head>
       <body>{children}</body>
     </html>
-  );
+  )
 }
