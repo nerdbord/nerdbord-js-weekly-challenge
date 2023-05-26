74bd71ce4ccb6f903a02312793d7decc925ff39c
bmbaron <benjamin.m.baron@gmail.com>
2023-05-26 18:30:02 +0700

    add animation

--- challenges/week-3/main.js
+++ challenges/week-3/main.js
@@ -1,8 +1,38 @@
-const middleContent = () => {
-    const mainDiv = document.createElement('div');
-    mainDiv.innerText = 'yo, wassup';
-    return mainDiv;
+
+const openButton = document.getElementById('open-button');
+const myModal = document.getElementById('myModal');
+const modal = document.getElementById('modal-container');
+const closeButton = document.getElementById('close-button');
+
+
+const handleOpen = () => {
+    modal.classList.add('animate');
+    const wait = setTimeout(() => {
+        document.body.addEventListener('click', () => {
+            const backdrop = document.querySelector('.modal-backdrop');
+            backdrop.style.display = 'none';
+            myModal.style.display = 'none';
+            modal.classList.remove('animate');
+        });
+    }, 1000)
 }
 
-const root = document.getElementById('root');
-root.append(middleContent());
+const handleClose = () => {
+    modal.classList.remove('animate');
+    document.body.removeEventListener('click', () => {
+    });
+
+}
+
+
+const handleAnimations = () => {
+    openButton.addEventListener('click', () => {
+        const myTimeout = setTimeout(handleOpen, 100);
+    });
+    closeButton.addEventListener('click', () => {
+        const myTimeout = setTimeout(handleClose, 100);
+    });
+}
+
+
+handleAnimations();
