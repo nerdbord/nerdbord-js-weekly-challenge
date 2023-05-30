c7fc7a4ed003a339a259fe5a183d9d0e38a8f0c9
AniaPiWo <snowflake@wp.pl>
2023-05-30 14:06:58 +0200

    db fix

--- challenges/week-4/InteractiveProductCard.jsx
+++ challenges/week-4/InteractiveProductCard.jsx
@@ -1,30 +1,42 @@
-import React, { useState } from "react";
+import React from "react";
 import css from "./InteractiveProductCard.module.css";
 
 export const InteractiveProductCard = () => {
-    const imagePaths = [
-        './challenges/week-4/img/Rectangle1.png',
-        './challenges/week-4/img/Rectangle2.png',
-        './challenges/week-4/img/Rectangle3.png',
-        './challenges/week-4/img/Rectangle4.png',
-        './challenges/week-4/img/Rectangle5.png',
-        './challenges/week-4/img/Rectangle6.png'
-    ];
+  const images = [
+    {
+      src: './challenges/week-4/img/Rectangle1.png',
+      text: 'Small plant'
+    },
+    {
+      src: './challenges/week-4/img/Rectangle2.png',
+      text: 'Long plant'
+    },
+    {
+      src: './challenges/week-4/img/Rectangle3.png',
+      text: 'Stylish plant'
+    },
+    {
+      src: './challenges/week-4/img/Rectangle4.png',
+      text: 'Glass plant'
+    },
+    {
+      src: './challenges/week-4/img/Rectangle5.png',
+      text: 'Standard plant'
+    },
+    {
+      src: './challenges/week-4/img/Rectangle6.png',
+      text: 'Cosy plant'
+    }
+  ];
 
-    const imageText = [
-        'Small plant', 'Long plant', 'Stylish plant', "Glass plant", 'Standard plant', 'Cosy plant'
-    ];
-
-    return (
-        <>
-            <div className={css.cardGrid}>
-                {imagePaths.map((imagePath, index) => (
-                    <div className={css.cardItem} key={index}>
-                        <p className={css.cardText}>{imageText[index]}</p>
-                        <img className={css.cardImg} src={imagePath} alt={`Rectangle ${index + 1}`} />
-                    </div>
-                ))}
-            </div>
-        </>
-    );
+  return (
+    <div className={css.cardGrid}>
+      {images.map((image, index) => (
+        <div className={css.cardItem} key={index}>
+          <p className={css.cardText}>{image.text}</p>
+          <img className={css.cardImg} src={image.src} alt={`Rectangle ${index + 1}`} />
+        </div>
+      ))}
+    </div>
+  );
 };
