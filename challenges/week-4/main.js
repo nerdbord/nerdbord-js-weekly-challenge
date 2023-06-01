72984c158c42501a8c4ec6acd7c80bd0748f8e1d
bmbaron <benjamin.m.baron@gmail.com>
2023-06-01 18:30:42 +0700

    fixed display of text from json

--- challenges/week-4/main.js
+++ challenges/week-4/main.js
@@ -1,9 +1,11 @@
 import cardData from './card-data.json' assert {type: 'json'};
 const populateCards = () => {
     const cards = document.getElementsByClassName('card');
+    const cardTextDivs = document.getElementsByClassName('card-text');
     const cardsArray = Array.from(cards);
+    const cardTextDivsArray = Array.from(cardTextDivs);
     cardsArray.map((card, index) => {
-        card.style.innerText = `url(${cardData[index].text})`;
+        cardTextDivsArray[index].innerText = cardData[index].text;
         card.style.backgroundImage = `url(${cardData[index].image.attributes.url})`;
     })
 }
