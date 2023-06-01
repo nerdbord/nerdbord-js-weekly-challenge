0d6179324f90576db33d6c0d5da84b944a77c523
kadheryna <katost95@gmail.com>
2023-06-01 21:33:32 +0200

    Complete task

---
+++ challenges/week-4/tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ["./src/**/*.{html,js}"],
+	theme: {
+		extend: {
+			colors: {
+				main: "#FAFAFA",
+				shadow: "#D9D9D9",
+			},
+			gridTemplateColumns: {
+				new1: "repeat(1, minmax(0, calc(300px - 40px)))",
+			},
+			keyframes: {
+				appearanceTop: {
+					"0%": { transform: "translateY(-1rem)" },
+					"100%": { transform: "translateY(0)" },
+				},
+			},
+			animation: {
+				appearanceTop: "appearanceTop 0.5s ease-in-out",
+			},
+		},
+	},
+	plugins: [],
+};
