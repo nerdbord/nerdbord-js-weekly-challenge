2bcfbe6692ad2872d2844000221d310bdc43d59d
Urszula Chrzanowska <124902071+radiodrac@users.noreply.github.com>
2023-06-03 03:11:48 +0200

    update styles.css, script.js - add open/close modal functionality

---
+++ challenges/week-3/script.js
@@ -0,0 +1,18 @@
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
+const openModalBtn = document.querySelector(".btn-open");
+const closeModalBtn = document.querySelector(".btn-close");
+
+function openModal() {
+    modal.classList.remove("hidden");
+    overlay.classList.remove("hidden");
+}
+
+function closeModal() {
+    modal.classList.add("hidden");
+    overlay.classList.add("hidden");
+}
+
+openModalBtn.addEventListener("click", openModal);
+
+closeModalBtn.addEventListener("click", closeModal);
