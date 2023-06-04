521371d615656fe9a29970b5a77851b87fb22b8f
AniaPiWo <snowflake@wp.pl>
2023-06-04 13:29:02 +0200

    img fix

--- challenges/week-4/InteractiveProductCard.jsx
+++ challenges/week-4/InteractiveProductCard.jsx
@@ -1,42 +1,26 @@
 import React from "react";
 import css from "./InteractiveProductCard.module.css";
+import { plants } from "./plants.mjs";
 
 export const InteractiveProductCard = () => {
-  const images = [
-    {
-      src: './public/img/Rectangle4.png',
-      text: 'Small plant'
-    },
-    {
-      src: './public/img/Rectangle2.png',
-      text: 'Long plant'
-    },
-    {
-      src: './public/img/Rectangle3.png',
-      text: 'Stylish plant'
-    },
-    {
-      src: './public/img//img/Rectangle4.png',
-      text: 'Glass plant'
-    },
-    {
-      src: './public/img/Rectangle5.png',
-      text: 'Standard plant'
-    },
-    {
-      src: './public/img/Rectangle6.png',
-      text: 'Cosy plant'
-    }
-  ];
 
   return (
     <div className={css.cardGrid}>
-      {images.map((image, index) => (
-        <div className={css.cardItem} key={index}>
-          <p className={css.cardText}>{image.text}</p>
-          <img className={css.cardImg} src={image.src} alt={`Rectangle ${index + 1}`} />
-        </div>
-      ))}
+
+      {plants.map((plant, index) => {
+        const imagePath = new URL(`./img/${plant.imageSrc}`, import.meta.url).href;
+        const imageAlt = plant.text;
+        
+        console.log(imagePath);
+        console.log(imageAlt);
+
+        return (
+          <div className={css.cardItem} key={index}>
+            <p className={css.cardText}>{plant.text}</p>
+            <img className={css.cardImg} src={imagePath} alt={imageAlt} />
+          </div>
+        );
+      })}
     </div>
   );
 };
---
+++ challenges/week-4/plants.mjs
@@ -0,0 +1,27 @@
+
+export const plants = [
+    {
+      imageSrc: "Rectangle1.png",
+      text: 'Small plant'
+    },
+    {
+      imageSrc: "Rectangle2.png",
+      text: 'Long plant'
+    },
+    {
+      imageSrc: "Rectangle3.png",
+      text: 'Stylish plant'
+    },
+    {
+      imageSrc: "Rectangle4.png",
+      text: 'Glass plant'
+    },
+    {
+      imageSrc: "Rectangle5.png",
+      text: 'Standard plant'
+    },
+    {
+      imageSrc: "Rectangle6.png",
+      text: 'Cosy plant'
+    }
+  ];
