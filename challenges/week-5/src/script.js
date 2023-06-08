2aab6549f165c665dc96241d4a4ff056c5a7df55
kadheryna <katost95@gmail.com>
2023-06-08 22:06:04 +0200

    Complete task from week-5 challange

---
+++ challenges/week-5/src/script.js
@@ -0,0 +1,10 @@
+const date = new Date();
+const day = date.getDate();
+const month = date.getMonth() + 1;
+const year = date.getFullYear();
+
+const currentData = `${day} / ${month} / ${year}`;
+console.log(currentData);
+
+const showData = document.querySelector(".showData");
+showData.textContent = currentData;
