87f5c8c201398dc3ba619a327678e91881a4bf1c
Karol Wiewiórka <karol.wiewiorka@gmail.com>
2023-06-12 12:50:58 +0200

    Final stage

--- script.js
+++ script.js
@@ -2,8 +2,10 @@ const icon = document.querySelector(".nav-icon");
 const bar1 = document.querySelector(".bar1");
 const bar2 = document.querySelector(".bar2");
 const bar3 = document.querySelector(".bar3");
+const navImg = document.querySelector(".nav-img");
 const bars = document.querySelectorAll(".nav-icon div");
 
 icon.addEventListener("click", () => {
     icon.classList.toggle("icon-active");
+    navImg.classList.toggle("nav-img-active");
 })
